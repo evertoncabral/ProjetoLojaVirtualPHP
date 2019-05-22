8366c09e94ac278baaa1eb5bda7c3835aab6cf1a
Everton Cabral <evertoncab@gmail.com>
2019-05-22 17:21:01 -0300

    ProjetoLojaVirtualPHP

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+    <?php 
+            $nome = "Everton";
+        if ($nome=="Everton"){
+            var_dump( $nome."<br>");
+        }else{
+            echo"não é o Everton";
+        }
+            
+        for ($i=0;$i<5;$i++){
+            echo"estou dentro do FOR" . "<br>";
+        }
+
+
+
+    ?>
+
+
+
+</body>
+</html>
