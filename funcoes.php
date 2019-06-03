47789e60fad4011b0b1cd46d6f201eff04b3c838
Everton Cabral <evertoncab@gmail.com>
2019-06-03 17:46:10 -0300

    Validando formulário de compra

--- funcoes.php
+++ funcoes.php
@@ -4,10 +4,32 @@ function logarUsuario($nome, $nivelDeAcesso){
     $usuario = ["logado"=>true,"nome"=>$nome,"nivelAcesso"=>$nivelDeAcesso];
     return $usuario;
 }
+
 function addProduto($nome,$descricao,$preco,$imagem,$produtos){
     $novoProduto = ["nome"=>$nome, "descricao"=>$descricao, "preco"=>$preco,"imagem"=>$imagem];
     $chave = count($produtos)+1;
     $produtos["produto$chave"]=$novoProduto;
     return $produtos;
 }
+
+function validarNome($nome){
+    return $nome!= "" && strlen($nome)>=3;
+}
+
+function validarCPF($cpf){
+    return is_integer($cpf) && count($cpf)==11;
+}
+
+function validarCartao($nrnCartao){
+    return is_integer($nrnCartao) && count($nrnCartao)==16;
+}
+
+function validarDataValidade($dataValidade){
+    return $dataValidade>date("y-m-d");
+}
+
+function validarCVV($cvv){
+    return  strlen($cvv)==3;
+}
+
 
--- sucesso.php
+++ sucesso.php
@@ -1,13 +1,29 @@
 <?php
-if ($_POST){
-    if($_POST["nomeCliente"]==""){
-        header("location:index.php");
+
+function validarCompra($dadosCompras){
+    $erros = [];
+        if(!$dadosCompras){
+            $erros[] = "Não foi recebido nenhum dado para realizar a compra!!";
+        }
+        if(!validarNome($dadosCompras["nomeCliente"])){
+            $erros[]="verifique se o seu nome está correto e se é maior do que 2 caracteres..!!";
+        }
+        if(!validarCPF($dadosCompras["cpfCliente"])){
+            $erros[]="CPF inválido..!! Tenta de novo!!";
+        }
+        if(!validarCartao($dadosCompras["cartaoCliente"])){
+            $erros[]="número de cartão inválido..!! Tenta de novo!!";
+        }
+        if(!validarDataValidade($dadosCompras["dataValidadeCartao"])){
+            $erros[]="data validade  de cartão inválido..!! Tenta de novo!!";
+        }
+        if(!validarCVV($dadosCompras["cvvCartao"])){
+            $erros[]="número de CVV inválido..!! Tenta de novo!!";
+        }
+        return $erros;
 }
-    $nomeCliente=$_POST["nomeCliente"];
-    $nomeProduto=$_POST["nomeProduto"];
-}else{
-    header("location:index.php");
-}
+
+
 
 ?>
 
@@ -21,15 +37,26 @@ if ($_POST){
 
 <main class="container">
     <section class="row">
+    
+    <?php
+    $errosValidacao = validarCompra($_POST);
+    if(count($errosValidacao)>0): ?>
+    <div class="col-md-12">
+    <ul>
+            <?php foreach($errosValidacao as $erro): ?>
+                    <li><?php echo $erro; ?></li>
+            <?php endforeach;?>
+    </ul>
+    
+    </div>
         <div class="col-md-12">
-        
-<div class="alert alert-success" role="alert">
-  Olá <?php echo $nomeCliente;?> parabéns pela sua compra do produto <?php echo $nomeProduto;?>
-</div>
-
-</div>
+            <div class="alert alert-success" role="alert">
+                 Olá <?php echo $_POST['nomeCliente'];?> parabéns pela sua compra do produto <?php echo $_POST['nomeProduto'];?>
+            </div>
+        </div>
+    <?php endif;?>
             <div class="col-md-12">
-            <a href="iindex.php" class="btn btn-primary">Voltar para Home!!</a>
+                <a href="iindex.php" class="btn btn-primary">Voltar para Home!!</a>
             </div>
     </section>
 </main>
